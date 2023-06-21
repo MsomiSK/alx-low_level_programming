4a7b9ea983aaf53dc9bcf6e7023cffbdc3ea0afc
msomisk <syabonga.msomisk@gmail.com>
2023-06-20 17:14:01 -0700

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,13 +3,13 @@
 /**
  * main - prints natural numbers below 1024 that are multiplies of 3 or 5
  *
- * Return: always 0
+ * Return: always 0.
  */
 int main(void)
 {
 	int a, b;
 
-	for (a - 1; a < 1024; a++)
+	for (a = 1; a < 1024; a++)
 	{
 		if ((a % 3) == 0 || (a % 5) == 0)
 			b += a;
