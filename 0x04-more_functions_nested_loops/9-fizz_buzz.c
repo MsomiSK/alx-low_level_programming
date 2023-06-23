0a89ade431b584a6fcf3df2a9c55d66b1d30e42a
msomisk <syabonga.msomisk@gmail.com>
2023-06-23 15:28:49 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 
 /**
- *main - FizzBuzz test is an interview designed to help filter out the 99.5%
- *
- *Description: program that prints numbers from 1 to 100, with  new line.
- *fizz for  multiples of 3 print Fizz and for the multiple of 5 print Buzz
- *For numbers which are multiples of both three and five print FizzBuzz.
- *
- * Return: 0
+ * main - prints buzz each numbers of thre and five
+ * Return: 0.
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 1; i <= 100; i++)
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
 	{
-		if (i % 15 == 0)
-			printf("Fizzbuzz");
-		else if (i % 3 == 0)
-			printf("fizz");
-		else if (i % 5 == 0)
-			printf("buzz");
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
 		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
+			printf("%d", n);
 	}
 	printf("\n");
 	return (0);
 }
+
